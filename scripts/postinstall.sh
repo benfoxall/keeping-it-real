d2cc882fdeb31d8445094f818fe980b26ca2499b
Ben Foxall <benfoxall@gmail.com>
2017-07-08 19:16:51 +0100

    update post install to use docs
    dunno about this script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p talk/lib
+mkdir -p docs/lib
 
-cp -r node_modules/reveal.js talk/lib/reveal.js
-cp -r node_modules/slide-builder/slide-builder.js talk/lib/slide-builder.js
+cp -r node_modules/reveal.js docs/lib/reveal.js
+cp -r node_modules/slide-builder/slide-builder.js docs/lib/slide-builder.js
